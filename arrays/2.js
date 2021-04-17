1ecdef83a8cd0302bb304c4e122ce0092a0b5ba0
Ali Gebai <ali.gebai@gmail.com>
2021-04-17 16:18:02 +0000

    pushing the arrays exercises to github

---
+++ arrays/2.js
@@ -0,0 +1,9 @@
+let myArray = [1, 3, 6, 11, 4, 2, 4, 9, 17, 16, 0];
+
+for (let i = 0; i < myArray.length; i += 1) {
+  if (myArray[i] % 2 === 0) {
+    console.log(myArray[i]);
+  }
+}
+
+touch 3.js
